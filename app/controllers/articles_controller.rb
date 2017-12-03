2f013fb1d3e8ec3b74877c2f38d72e8ec8ebe30d
Tim <tim.lin@letote.cn>
2017-12-03 23:14:27 +0800

    add edit and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,11 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def show
@@ -15,12 +20,25 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
 
-    @article.save
-    redirect_to @article
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
   end
 
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
   private
     def article_params
       params.require(:article).permit(:title, :text)
     end
+
 end
